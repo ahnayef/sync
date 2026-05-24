57a50d0682d476877775870358ad5c8a9cab90bc
AHN <ahndo7059@gmail.com>
2026-05-24 19:27:56 +0600

    Disable on dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
-import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 
 const inter = Inter({
